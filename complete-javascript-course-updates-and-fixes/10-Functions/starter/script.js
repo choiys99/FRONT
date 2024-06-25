dffd2710cb46d0b31ac47212b98b5ddb0aedcdf9
choiys99 <cys1849908@gmail.com>
2024-06-25 22:43:49 +0900

    js

--- complete-javascript-course-updates-and-fixes/10-Functions/starter/script.js
+++ complete-javascript-course-updates-and-fixes/10-Functions/starter/script.js
@@ -105,6 +105,7 @@ const lufthansa = {
   bookings: [],
   // book:function(){}
   book(flightNum, name) {
+    // 객체의 메소드 1
     console.log(
       `${name} book a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
     );
@@ -116,14 +117,102 @@ lufthansa.book(635, 'John Smith');
 // console.log(lufthansa);
 
 const eurowings = {
-  name: 'Eurowings',
+  airline: 'Eurowings',
   iataCode: 'EW',
   bookings: [],
 };
 
-const book = lufthansa.book;
+const book = lufthansa.book; // 2 하지만 여기서 일반함수가 되어버림 더이상 1을 가르키지않음
 
 // book(23, 'Sarah Williams'); // 안됨 이젠 book은 일반함수라서 기능을 작동못함 this 키워드가 작동안함
 
 book.call(eurowings, 23, 'Sarah Williams'); // call() = this 가 가르키고자하느거 ,
 console.log(eurowings);
+
+book.call(lufthansa, 239, 'Mary Cooper');
+console.log(eurowings);
+
+const swiss = {
+  airline: 'Swiss Air Lines',
+  iataCode: 'LX',
+  bookings: [],
+};
+
+book.call(swiss, 583, 'Mary Copper');
+console.log(swiss);
+
+//Apply
+const flightData = [583, 'George Cooper'];
+// book.apply(swiss, flightData); // apply메소드는 두번째는 배열로 받는다
+console.log(swiss);
+
+book.call(swiss, ...flightData); // 스프레드연산자 사용으로 더 효율적
+
+const bookEw23 = book.bind(eurowings, 23); // bind() 메서드를 사용하여 book 함수의 this를 eurowings로 고정, 그리고 flightNum을 23으로 고정
+bookEw23('Jonas Schmedtmann'); // bookEw23 함수 실행, 이때 name만 전달하면 됨
+bookEw23('Martha Cooper');
+
+// 이벤트 리스너 활ㅇㅇ
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this); // 이벤트 리스너 안의 this는 해당 이벤트가 발생한 dom 객체를 가르키기 때문에 this는 버튼객체를참조
+
+  this.planes++;
+  console.log(this.planes); // 안나옴 ㅇㅇ
+};
+// lufthansa.buyPlane();
+
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// Partial application
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23); // addtax 화살표함수 사용하고있어서 첫번째 인자 null 사용
+// addVat = value => value + value * 0.23
+
+// console.log(addVAT(100));
+
+const poll = {
+  question: `What is your favourite programming language?`,
+  options: ['0: JavaScript', '1: Python', '2: rust', '3: C++'],
+
+  answers: new Array(4).fill(0), // 결과를 저장하는 배열 , 초기값은 모두 0
+  registerNewAnswer() {
+    // 사용자로부터 투표응답을 받는 메서드
+    const answer = Number(
+      prompt(
+        `${this.question}\n${this.options.join('\n')} \n (Write option number)`
+      )
+    );
+    console.log(answer);
+
+    //타입
+    typeof answer === 'number' &&
+      answer < this.answers.length &&
+      this.answers[answer]++;
+
+    // console.log(this.answers);
+    this.displayResults();
+    this.displayResults('string');
+  },
+  displayResults(type = 'array') {
+    // 기본값으로 array를 가짐
+    if (type === 'array') {
+      console.log(this.answers);
+    } else if (type === 'string') {
+      console.log(`Poll results are${this.answers.join(', ')}`);
+    }
+  },
+};
+
+// poll.registerNewAnswer();
+
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+poll.displayResults.call({ answers: [5, 2, 3] });
+poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
