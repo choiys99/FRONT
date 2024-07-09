3e94094c439524404e71d348c815ffa3af78285a
choiys99 <cys1849908@gmail.com>
2024-07-09 17:01:02 +0900

    js 슬라이드

--- complete-javascript-course-updates-and-fixes/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course-updates-and-fixes/13-Advanced-DOM-Bankist/starter/script.js
@@ -267,7 +267,7 @@ const sectionObserver = new IntersectionObserver(revealSection, {
 
 allSections.forEach(function (section) {
   sectionObserver.observe(section); // 각 섹션을 sectionObserver에 등록하여 관찰하도록 합니다.
-  section.classList.add('section--hidden'); // 모든 섹션에 'section--hidden' 클래스를 추가하여 초기에 숨겨둡니다.
+  // section.classList.add('section--hidden'); // 모든 섹션에 'section--hidden' 클래스를 추가하여 초기에 숨겨둡니다.
 });
 
 // 레이지로딩 이미지
@@ -296,6 +296,98 @@ const imgObserver = new IntersectionObserver(loadImg, {
 });
 
 imgTargets.forEach(img => imgObserver.observe(img));
+
+//슬라이더
+
+const sliders = function () {
+  const slides = document.querySelectorAll('.slide'); // 슬라이더
+
+  const btnLeft = document.querySelector('.slider__btn--left'); // 슬라이더 버튼
+  const btnRight = document.querySelector('.slider__btn--right'); // 버튼
+  let curSlide = 0;
+  const maxSlide = slides.length;
+
+  const dotContainer = document.querySelector('.dots');
+
+  // 슬라이더 동그라미 그거
+  const createDots = function () {
+    slides.forEach(function (_, i) {
+      dotContainer.insertAdjacentHTML(
+        'beforeend',
+        `<button class ="dots__dot" data-slide="${i}"></button>`
+      );
+    });
+  };
+
+  // const slider = document.querySelector('.slider');
+  // slider.style.transform = 'scale(0.4) translateX(-800px)';
+  // slider.style.overflow = 'visible';
+
+  //첫번째슬라이드 0퍼 두번째 100 200 300 %
+  // slides.forEach((s, i) => (s.style.transform = `translateX(${100 * i}%)`));
+
+  //슬라이드 이동
+  const goToSlide = function (slide) {
+    slides.forEach(
+      (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+    );
+  };
+
+  const activateDot = function (slide) {
+    document
+      .querySelectorAll('.dots__dot')
+      .forEach(dot => dot.classList.remove('dots__dot--active'));
+
+    document
+      .querySelector(`.dots__dot[data-slide="${slide}"]`)
+      .classList.add('dots__dot--active');
+  };
+
+  const nextSlide = function () {
+    if (curSlide === maxSlide - 1) {
+      curSlide = 0;
+    } else {
+      curSlide++;
+    }
+    goToSlide(curSlide);
+    activateDot(curSlide);
+  };
+  const prevSlide = function () {
+    if (curSlide === 0) {
+      curSlide = maxSlide - 1;
+    } else {
+      curSlide--;
+    }
+    goToSlide(curSlide);
+    activateDot(curSlide);
+  };
+
+  const init = function () {
+    goToSlide(0);
+    createDots();
+
+    activateDot(0);
+  };
+  init();
+
+  btnRight.addEventListener('click', nextSlide);
+  btnLeft.addEventListener('click', prevSlide);
+
+  document.addEventListener('keydown', function (e) {
+    console.log(e);
+    if (e.key === 'ArrowLeft') prevSlide();
+    e.key === 'ArrowRight' && nextSlide();
+  });
+
+  dotContainer.addEventListener('click', function (e) {
+    if (e.target.classList.contains('dots__dot')) {
+      const { slide } = e.target.dataset;
+      goToSlide(slide);
+      activateDot(slide);
+    }
+  });
+};
+sliders();
 
 ////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////
