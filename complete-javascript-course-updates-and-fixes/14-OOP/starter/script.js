fc8dedec9014103fba5dacf208b4ca23dcea492d
choiys99 <cys1849908@gmail.com>
2024-07-12 18:07:02 +0900

    js 공개 비공개 메소드

--- complete-javascript-course-updates-and-fixes/14-OOP/starter/script.js
+++ complete-javascript-course-updates-and-fixes/14-OOP/starter/script.js
@@ -83,51 +83,51 @@ bmw.accelerate();
 // const PersonCl2 = class {}
 
 //클래스 선언식
-class PersonCl {
-  constructor(fullName, birthYear) {
-    this.fullName = fullName;
-    this.birthYear = birthYear;
-  }
+// class PersonCl {
+//   constructor(fullName, birthYear) {
+//     this.fullName = fullName;
+//     this.birthYear = birthYear;
+//   }
 
-  // Instance methods
-  // Methods will be added to .prototype property
-  calcAge() {
-    console.log(2037 - this.birthYear);
-  }
+//   // Instance methods
+//   // Methods will be added to .prototype property
+//   calcAge() {
+//     console.log(2037 - this.birthYear);
+//   }
 
-  greet() {
-    console.log(`Hey ${this.fullName}`);
-  }
+//   greet() {
+//     console.log(`Hey ${this.fullName}`);
+//   }
 
-  get age() {
-    return 2037 - this.birthYear;
-  }
+//   get age() {
+//     return 2037 - this.birthYear;
+//   }
 
-  // Set a property that already exists
-  set fullName(name) {
-    if (name.includes(' ')) this._fullName = name;
-    // else alert(`${name} is not a full name!`);
-  }
+//   // Set a property that already exists
+//   set fullName(name) {
+//     if (name.includes(' ')) this._fullName = name;
+//     // else alert(`${name} is not a full name!`);
+//   }
 
-  get fullName() {
-    return this._fullName;
-  }
+//   get fullName() {
+//     return this._fullName;
+//   }
 
-  // Static method
-  static hey() {
-    console.log('Hey there 👋');
-    console.log(this);
-  }
-}
+//   // Static method
+//   static hey() {
+//     console.log('Hey there 👋');
+//     console.log(this);
+//   }
+// }
 
-const jessica = new PersonCl('Jessica', 1999); //생성자 내부의 this 키워드도 새로 생성된 빈 개체로 설정
-console.log(jessica.__proto__ === PersonCl.prototype);
+// const jessica = new PersonCl('Jessica', 1999); //생성자 내부의 this 키워드도 새로 생성된 빈 개체로 설정
+// console.log(jessica.__proto__ === PersonCl.prototype);
 
 // PersonCl.prototype.greet = function () {
 //   console.log(`hey${this.firstName}`);
 // };
 
-jessica.greet();
+// jessica.greet();
 
 // 1. 클래스는 호이스팅안됨
 // 2. 첫글자는 대문자
@@ -144,7 +144,7 @@ const account = {
   },
 };
 // console.log(PersonCl.hey);
-PersonCl.hey();
+// PersonCl.hey();
 
 // Object.create
 
@@ -173,33 +173,33 @@ sarah.init('Sarah', 1979);
 sarah.calcAge();
 
 //문제해결
-class CarCl {
-  constructor(make, speed) {
-    this.make = make;
-    this.speed = speed;
-  }
-  accelerate() {
-    this.speed += 10;
-    console.log(`${this.make} is going at ${this.speed} km/h`);
-  }
+// class CarCl {
+//   constructor(make, speed) {
+//     this.make = make;
+//     this.speed = speed;
+//   }
+//   accelerate() {
+//     this.speed += 10;
+//     console.log(`${this.make} is going at ${this.speed} km/h`);
+//   }
 
-  brake() {
-    this.speed -= 10;
-    console.log(`${this.make} is going at ${this.speed} km/h`);
-  }
-  get speedUs() {
-    return this.speed / 1.6;
-  }
-  set speedUs(speed) {
-    this.speed = speed * 1.6;
-  }
-}
-const ford = new CarCl('Ford', 120);
+//   brake() {
+//     this.speed -= 10;
+//     console.log(`${this.make} is going at ${this.speed} km/h`);
+//   }
+//   get speedUs() {
+//     return this.speed / 1.6;
+//   }
+//   set speedUs(speed) {
+//     this.speed = speed * 1.6;
+//   }
+// }
+// const ford = new CarCl('Ford', 120);
 
-console.log(ford.speedUs);
-ford.accelerate();
-ford.speedUs = 50;
-console.log(ford);
+// console.log(ford.speedUs);
+// ford.accelerate();
+// ford.speedUs = 50;
+// console.log(ford);
 
 const Student = function (firstName, birthYear, course) {
   this.firstName = firstName;
@@ -231,3 +231,209 @@ Student.prototype.constructor = Student;
 console.log(Student.prototype.constructor);
 
 console.dir(mike.__proto__.__proto__.constructor);
+
+//////////////////////////////////////
+
+const Car1 = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+}; // 생성자 함수 car1 make speed 를 받음
+
+Car1.prototype.accelerate = function () {
+  // 메소드 추가 실행되면 스피드 + 10
+  this.speed += 10;
+  console.log(`${this.make} is goind at ${this.speed} km/h`);
+};
+
+Car1.prototype.brake = function () {
+  // 메소드 추가 스피ㄷ, -5
+  this.speed -= 5;
+  console.log(`${this.make} is goind at ${this.speed} km/h`);
+};
+
+const EV = function (make, speed, charge) {
+  Car1.call(this, make, speed);
+  this.charge = charge; // 이렇게하면 ev 객체를 가르킴
+};
+
+EV.prototype = Object.create(Car1.prototype);
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge--;
+  console.log(`${this.make} is goind at ${this.speed} km/h
+    with a charge of ${this.charge}`);
+};
+const tesla = new EV('테슬라', 120, 23);
+console.log(tesla);
+tesla.chargeBattery(90);
+console.log(tesla);
+tesla.brake();
+tesla.accelerate();
+
+class PersonCl {
+  constructor(fullName, birthYear) {
+    // 생성자 메서드, 객체 생성 시 fullName, birthYear 속성 초기화
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+
+  // 인스턴스 메서드, prototype 프로퍼티에 추가됨
+  calcAge() {
+    // 현재 년도(2037)에서 birthYear를 빼서 나이 계산 후 콘솔에 출력
+    console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    // fullName 속성을 사용하여 인사말 콘솔에 출력
+    console.log(`Hey ${this.fullName}`);
+  }
+
+  get age() {
+    // 나이 계산 후 반환하는 게터 메서드
+    return 2037 - this.birthYear;
+  }
+
+  // 기존 속성에 대한 세터 메서드
+  set fullName(name) {
+    if (name.includes(' ')) this._fullName = name;
+    // else alert(`${name} is not a full name!`);
+  }
+
+  get fullName() {
+    // _fullName 속성을 반환하는 게터 메서드
+    return this._fullName;
+  }
+
+  // 정적 메서드, 클래스 자체에서 호출
+  static hey() {
+    console.log('Hey there 👋');
+    console.log(this); // this는 PersonCl 클래스를 가리킴
+  }
+}
+
+class StudentCl extends PersonCl {
+  constructor(fullName, birthYear, course) {
+    // PersonCl 클래스의 constructor 호출하여 fullName, birthYear 초기화
+    super(fullName, birthYear);
+    this.course = course; // course 속성 추가 초기화
+  }
+  includes() {
+    // fullName, course 속성을 사용하여 정보 출력
+    console.log(
+      `저의 이름은 ${this.fullName} 그리고 직업은 ${this.course} 입니다`
+    );
+  }
+  calcAge() {
+    // 부모 클래스의 calcAge 메서드 오버라이드
+    console.log(`나이는 ${this.age} 입니다`);
+  }
+}
+
+const martha = new StudentCl('최예성', 2012, '알바생');
+martha.includes();
+martha.calcAge(); // 부모 클래스의 calcAge 메서드가 오버라이드 됨
+
+// PersonProto2 객체, 프로토타입 객체
+const PersonProto2 = {
+  // calcAge 메서드, 현재 년도(2024)에서 birthYear를 빼서 나이 계산 후 콘솔에 출력
+  calcAge() {
+    console.log(2024 - this.birthYear);
+  },
+  // init 메서드, firstName, birthYear 속성 초기화
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+// PersonProto2 객체를 프로토타입으로 하는 steven2 객체 생성
+const steven2 = Object.create(PersonProto2);
+
+// PersonProto2 객체를 프로토타입으로 하는 StudentProto2 객체 생성
+const StudentProto2 = Object.create(PersonProto2);
+
+// StudentProto2 객체의 init 메서드 오버라이드, PersonProto2의 init 메서드 호출 후 course 속성 추가
+StudentProto2.init = function (firstName, birthYear, course) {
+  PersonProto2.init.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+// StudentProto2 객체를 프로토타입으로 하는 jay2 객체 생성 후 init 메서드 호출
+const jay2 = Object.create(StudentProto2);
+jay2.init('jay', 2010, '알바');
+
+//////////////////////////////////////
+//////////////////////////////////////
+//////////////////////////////////////
+class Account {
+  // 1) public fields (instances)<< 프로토 타입 추가 x
+  locale = navigator.language;
+
+  // 2) Private fields
+  #movements = []; //#붙이는걸로 필드를 비공개로 만듬
+  #pin;
+
+  constructor(owner, currency, pin) {
+    this.owner = owner; // 계좌 소유자 이름
+    this.currency = currency; // 통화 종류
+    // protected property
+    this.#pin = pin; // PIN 코드
+    // pin은 입력값을 기반으로 생성자에 대한 핀을 설정
+    // 근데 생성자에서는 필드를 정의 할 수 없음
+
+    // this._movements = []; // 거래 내역 배열
+    // this.locale = navigator.language; // 사용자 언어 설정
+
+    console.log(`thanks for opeing an account, ${owner}`); // 계좌 개설 메시지 출력
+  }
+
+  // public interface << 항상 프로토 타입추가
+
+  // 3) public methods
+  getMovements() {
+    return this.#movements;
+  }
+  deposit(val) {
+    this.#movements.push(val); // 입금 금액을 movements 배열에 추가
+  }
+
+  withdraw(val) {
+    this.deposit(-val); // 출금 금액을 음수로 deposit 메서드 호출
+  }
+
+  requestLoan(val) {
+    if (this.#approveLoan(val)) {
+      // 대출 승인 여부 확인
+      this.deposit(val); // 대출 금액을 입금
+      console.log('화긴요'); // 대출 승인 메시지 출력
+    }
+  }
+
+  static helper() {
+    console.log('스태틱의단검');
+  }
+  // 4) private methods
+  #approveLoan(val) {
+    return true; // 대출 승인 여부를 반환 (항상 true)  하지만 구글 크롬에서는 사용불가
+  }
+}
+
+const acc1 = new Account('최예성', 'Kr', 111); // Account 클래스의 인스턴스 생성
+console.log(acc1); // 생성된 인스턴스 출력
+
+acc1.deposit(250); // 250원 입금
+acc1.withdraw(120); // 120원 출금
+acc1.requestLoan(1000);
+// acc1.approveLoan(1000); 허용하면안됨..
+console.log(acc1.getMovements());
+console.log(acc1); // 변경된 인스턴스 출력
+
+Account.helper();
+
+// console.log(acc1.#pin);
+// console.log(acc1.#movements); << 에러발생!! 찾을수없ㄷ!! 외부에서 접근이 불가능하다!
+
+// console.log(acc1.#approveLoan(100));
