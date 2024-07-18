12d4839094de4b1fe88a73b1f2b754e139cdb6f6
choiys99 <cys1849908@gmail.com>
2024-07-18 13:49:49 +0900

    js 예외처리

--- complete-javascript-course-updates-and-fixes/16-Asynchronous/starter/script.js
+++ complete-javascript-course-updates-and-fixes/16-Asynchronous/starter/script.js
@@ -3,6 +3,35 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+const renderCountry = function (data, className = '') {
+  const name = data.name.common;
+  const flag = data.flags.svg;
+  const region = data.region;
+  const language = Object.values(data.languages)[0];
+  const currency = Object.values(data.currencies)[0].name;
+
+  const html = `
+  <article class="country ${className}">
+    <img class="country__img" src="${flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${name}</h3>
+      <h4 class="country__region">${region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣️</span>${language}</p>
+      <p class="country__row"><span>💰</span>${currency}</p>
+    </div>
+  </article>
+  `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+  countriesContainer.style.opacity = 1;
+};
 ///////////////////////////////////////
 // https://github.com/public-apis/public-apis?tab=readme-ov-file#music
 
@@ -49,30 +78,6 @@ const countriesContainer = document.querySelector('.countries');
 // getCountryData('usa');
 // getCountryData('germany');
 
-const renderCountry = function (data, className = '') {
-  const name = data.name.common;
-  const flag = data.flags.svg;
-  const region = data.region;
-  const language = Object.values(data.languages)[0];
-  const currency = Object.values(data.currencies)[0].name;
-  const html = `
-  <article class="country ${className}">
-    <img class="country__img" src="${flag}" />
-    <div class="country__data">
-      <h3 class="country__name">${name}</h3>
-      <h4 class="country__region">${region}</h4>
-      <p class="country__row"><span>👫</span>${(
-        +data.population / 1000000
-      ).toFixed(1)} people</p>
-      <p class="country__row"><span>🗣️</span>${language}</p>
-      <p class="country__row"><span>💰</span>${currency}</p>
-    </div>
-  </article>
-  `;
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
-};
-
 const getCountryAndNeighbour = function (country) {
   // 첫번째 ajax 호출
   const request = new XMLHttpRequest();
@@ -90,6 +95,7 @@ const getCountryAndNeighbour = function (country) {
 
     // get neighbour country 2
     const [neighbour] = data.borders;
+    // console.log(neighbour);
 
     if (!neighbour) return; // 국경? 이웃나라가 없는경우
 
@@ -114,25 +120,102 @@ const getCountryAndNeighbour = function (country) {
 //  Promise 사용하기
 // 장점 비동기 결과를 처리하기 위해 더 이상 이벤트와 콜백함수에 의존할필요가없어짐
 
-//  Pending : 대기상태 = 아직 비동기 처리 로직이 완료 되지 않은상태
+// Pending : 대기상태 = 아직 비동기 처리 로직이 완료 되지 않은상태
 // Fulfilled  : 이행상태 = 비동기 처리 로직이 성공적으로 완료 됬다는 표현
 // Rejected  : 실패상태 = 비동기 로직이 실패
-const request = fetch('https://restcountries.com/v3.1/name/korea');
+// const request = fetch('https://restcountries.com/v3.1/name/korea');
 // console.log(request);
 
+const getJSON = function (url, errorMsg = '잘못했으') {
+  return fetch(url).then(response => {
+    if (!response.ok) {
+      throw new Error(`${errorMsg} (${response.status})`);
+    } // 이함수는 실제로 프로미스반환
+
+    return response.json();
+  });
+};
+
+// const getCountryData = function (country) {
+//   // Country 1
+//   fetch(`https://restcountries.com/v3.1/name/${country}`)
+//     .then(response => {
+//       console.log(response);
+
+//       if (!response.ok)
+//         throw new Error(`찾을 수 없는 나라입니다 (${response.status})`); // 응답 200이아닌경우 예외처리
+
+//       return response.json(); // 200일경우 문제없이 리턴
+//     })
+//     .then(data => {
+//       renderCountry(data[0]);
+//       // const neighbour = data[0].borders[0];
+//       const neighbour = 'dfsdfdef';
+
+//       if (!neighbour) return;
+
+//       // Country 2
+//       return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+//     })
+//     .then(response => {
+//       if (!response.ok)
+//         throw new Error(`찾을 수 없는 나라입니다. (${response.status})`);
+
+//       return response.json();
+//     })
+//     .then(data => {
+//       [data] = data;
+//       renderCountry(data, 'neighbour');
+//     })
+//     .catch(err => {
+//       // 해당 메소드내 오류 다잡음
+//       console.error(`${err} 💥💥💥`);
+//       renderError(`에러발생에러발생 💥💥 ${err.message}. 다시시도혀!`);
+//     })
+//     .finally(() => {
+//       // 성공 실패 여부 상관없이 무조건 실행
+//       countriesContainer.style.opacity = 1;
+//     });
+// };
+// // getCountryData('korea');
+
 const getCountryData = function (country) {
-  fetch(`https://restcountries.com/v3.1/name/${country}`)
-    // .then(function (response) {
-    //   // fetch 사용하면 즉시 Promise 가 반환
-    //   return response.json(); // 비동기함수 json
-    // })
-    // .then(function (data) {
-    //   // data = return 값
-    //   renderCountry(data[0]);
-    // });
-    .then(response => response.json())
-    .then(function (data) {
+  // Country 1
+  getJSON(
+    `https://restcountries.com/v3.1/name/${country}`,
+    '나라를 찾지 못했어요'
+  )
+    .then(data => {
       renderCountry(data[0]);
+      const neighbour = data[0].borders?.[0]; //?. = 접근값이 null 이거나 undef일 경우 에러 없이 und 반환
+
+      console.log(neighbour);
+
+      if (!neighbour) {
+        // 값이 없을경우 실행
+        throw new Error('인접한 국가가 없습니다.');
+      }
+
+      // Country 2
+      return getJSON(
+        `https://restcountries.com/v3.1/alpha/${neighbour}`,
+        '나라를 찾지 못했어요'
+      );
+    })
+
+    .then(data => {
+      [data] = data;
+      renderCountry(data, 'neighbour');
+    })
+    .catch(err => {
+      console.error(`${err} 💥💥💥`);
+      renderError(`에러에러 💥💥 ${err.message}. 다시시도혀!`);
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
     });
 };
-getCountryData('korea');
+
+btn.addEventListener('click', function () {
+  getCountryData('japan');
+});
