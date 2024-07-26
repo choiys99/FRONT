cf8bdefec715f450d78de80753265ed2c22db86e
choiys99 <cys1849908@gmail.com>
2024-07-26 19:58:27 +0900

    js 클린코드

--- complete-javascript-course-updates-and-fixes/17-Modern-JS-Modules-Tooling/starter/clean.js
+++ complete-javascript-course-updates-and-fixes/17-Modern-JS-Modules-Tooling/starter/clean.js
@@ -47,7 +47,10 @@ async/await 사용하고 then 및 catch 사용말고? promise 사용
 항상 예외처리는 필수
 */
 
-var budget = [
+// import { get } from "core-js/core/dict";
+
+"use strict ";
+const budget = Object.freeze([
   { value: 250, description: "Sold old TV 📺", user: "jonas" },
   { value: -45, description: "Groceries 🥑", user: "jonas" },
   { value: 3500, description: "Monthly salary 👩‍💻", user: "jonas" },
@@ -56,58 +59,72 @@ var budget = [
   { value: -20, description: "Candy 🍭", user: "matilda" },
   { value: -125, description: "Toys 🚂", user: "matilda" },
   { value: -1800, description: "New Laptop 💻", user: "jonas" },
-];
+]);
 
-var limits = {
+const spendingLimits = Object.freeze({
   jonas: 1500,
   matilda: 100,
+});
+
+const getLimit = (limits, user) => limits?.[user] ?? 0; // 지출한도 가져오기
+
+//pure function
+const addExpense = function (
+  state, //현재상태
+  limits, //지출 한도 객체
+  value, // 지불할 금액
+  description, //지출 설명
+  user = "jonas" //기본값 jonas 인 사용자의 이름
+) {
+  const cleanUser = user.toLowerCase();
+
+  return value <= getLimit(limits, cleanUser)
+    ? [...state, { value: -value, description, user: cleanUser }] //배열전개 연산자
+    : state;
 };
 
-var add = function (value, description, user) {
-  if (!user) user = "jonas";
-  user = user.toLowerCase();
+const newBudget1 = addExpense(budget, spendingLimits, 10, "Pizza 🍕");
+const newBudget2 = addExpense(
+  newBudget1,
+  spendingLimits,
+  100,
+  "Going to movies 🍿",
+  "Matilda"
+);
+const newBudget3 = addExpense(newBudget2, spendingLimits, 200, "Stuff", "Jay");
 
-  var lim;
-  if (limits[user]) {
-    lim = limits[user];
-  } else {
-    lim = 0;
-  }
+// const checkExpenses = function (state, limits) {
+//   return state.map((entry) => {
+//     return entry.value < -getLimit(limits, entry.user)
+//       ? { ...entry, flag: "limit" }
+//       : entry;
+//   });
+// };
 
-  if (value <= lim) {
-    budget.push({ value: -value, description: description, user: user });
-  }
+const checkExpenses = (state, limits) =>
+  state.map((entry) =>
+    entry.value < -getLimit(limits, entry.user)
+      ? { ...entry, flag: "limit" }
+      : entry
+  );
+
+const finalBudget = checkExpenses(newBudget3, spendingLimits);
+console.log(finalBudget);
+
+const logBigExpenses = function (state, bigLimit) {
+  const bigExpenses = state
+    .filter((entry) => entry.value <= -bigLimit)
+    .map((entry) => entry.description.slice(-2))
+    .join(" / ");
+  // .reduce((str, cur) => `${str} / ${cur.description.slice(-2)}`, " ");
+  console.log(bigExpenses);
+  // let output = "";
+  // for (const entry of budget) {
+  //   output +=
+  //     entry.value <= -bigLimit ? `${entry.description.slice(-2)} / ` : "";
+  // }
+  // output = output.slice(0, -2); // Remove last '/ '
+  // console.log(output);
 };
-add(10, "Pizza 🍕");
-add(100, "Going to movies 🍿", "Matilda");
-add(200, "Stuff", "Jay");
 console.log(budget);
-
-var check = function () {
-  for (var el of budget) {
-    var lim;
-    if (limits[el.user]) {
-      lim = limits[el.user];
-    } else {
-      lim = 0;
-    }
-
-    if (el.value < -lim) {
-      el.flag = "limit";
-    }
-  }
-};
-check();
-
-console.log(budget);
-
-var bigExpenses = function (limit) {
-  var output = "";
-  for (var el of budget) {
-    if (el.value <= -limit) {
-      output += el.description.slice(-2) + " / "; // Emojis are 2 chars
-    }
-  }
-  output = output.slice(0, -2); // Remove last '/ '
-  console.log(output);
-};
+logBigExpenses(finalBudget, 500);
